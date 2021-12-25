a4f61a81d0b38ef57f4c71d1b9a260d2118d073b
edikV <edward.vishnivetzki@gmail.com>
2021-12-25 13:30:05 +0200

    new

---
+++ training/Introduction/src/CoreJavaBrushup1.java
@@ -0,0 +1,16 @@
+public class CoreJavaBrushup1 {
+    public static void main(String[] args) {
+        /* Auto generated method stub */
+
+        int num=5;
+        String website="Rahul Shetty Academy";
+        char letter ='r';
+        double num2=5.9;
+
+        System.out.println(num + " is the stored value" + website);
+
+
+
+
+
+    }}
