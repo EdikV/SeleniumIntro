70c1b9213c8c44516b24f0b7b435175800ece383
edikV <edward.vishnivetzki@gmail.com>
2022-01-02 20:01:43 +0200

    new

--- training/Introduction/src/CoreJavaBrushup2.java
+++ training/Introduction/src/CoreJavaBrushup2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CoreJavaBrushup2 {
     public static void main(String[] args) {
 
@@ -7,16 +9,26 @@ public class CoreJavaBrushup2 {
 
 
 
-        for (int s:arr2) {
-            if (arr2[s] % 2 == 0) {
-                System.out.println(arr2[s]);
-                // if you want to break a loop, just use the brake;
-            }
-            else
-            {
-                System.out.print(arr2[s]+" Odd nubmers");
-            }
-        }
+     //   for (int s:arr2) {
+     //       if (arr2[s] % 2 == 0) {
+     //           System.out.println(arr2[s]);
+     //           // if you want to break a loop, just use the brake;
+     //       }
+     //       else
+     //       {
+     //           System.out.print(arr2[s]+" Odd nubmers");
+     //       }
+     //   }
+
+        //ArrayList- if you want to populate an array with a dynamic array size, you need to use
+        //ArrayList
+        ArrayList a = new ArrayList();
+        // create object of the class - object.method here a is the object
+
+        a.add("Rahul");
+        a.add("Edward");
+        System.out.print(a.get(1));
+
 
 
 
---
+++ training/Introduction/src/CoreJavaBrushup3.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+
+public class CoreJavaBrushup3 {
+    public static void main(String[] args) {
+        //String in Java is an object
+
+        //  String s2= new String("Hello \n Peter Parker");
+        //  You can choose how to split a string with split command
+        //  String[] splitted = s2.split("Peter");
+        //  System.out.print(splitted[0]);
+        //  If you want to trim excess spaces from a string you can use the s.trim()
+        //  System.out.print(splitted[1].trim());
+        //  Print each character
+        //  for (int i=0; i<s2.length();i++)
+        //  {
+            //      System.out.print(s2.charAt(i));
+
+            //  }
+        //  String txt = "Please locate where 'locate' occurs!";
+        //  System.out.println(txt.indexOf("locate"));
+
+        String txt ="abcdefghijklmno";
+        for (int i=0; i<txt.length(); i++){
+            System.out.println(txt.charAt(i));
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
