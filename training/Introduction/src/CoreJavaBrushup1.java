3540b49b7dad1149331787ac19f59fa562eeafc4
edikV <edward.vishnivetzki@gmail.com>
2022-01-02 12:47:18 +0200

    new

--- training/Introduction/src/CoreJavaBrushup1.java
+++ training/Introduction/src/CoreJavaBrushup1.java
@@ -9,6 +9,37 @@ public class CoreJavaBrushup1 {
 
         System.out.println(num + " is the stored value" + website);
 
+        //Arrays - Storing multiple values under a singe variabe.
+
+        // if you don't know the values of the array it is better to allocate space and then populate the array
+        int[] arr = new int[5]; // We declared an array of integers int[], and we define it with size of 5
+        arr[0] = 1;
+        arr[1] = 2;
+
+        //Method #2 of array declaration - if the values are already known
+        int[] arr2 = {1,2,3,4,5,6};
+
+        System.out.print(arr2[1]);
+
+    //   //Loops
+    //   for (int i=0;i< arr2.length;i++) {
+    //       System.out.print(arr2[i]);
+
+    //   }
+
+        //Enhanced for loops
+
+        for (int s:arr2){
+            System.out.print("");
+            System.out.println(s);
+        }
+
+
+
+
+        
+
+
 
 
 
---
+++ training/Introduction/src/CoreJavaBrushup2.java
@@ -0,0 +1,29 @@
+public class CoreJavaBrushup2 {
+    public static void main(String[] args) {
+
+
+        //Method #2 of array declaration - if the values are already known
+        int[] arr2 = {1,2,3,4,5,6};
+
+
+
+        for (int s:arr2) {
+            if (arr2[s] % 2 == 0) {
+                System.out.println(arr2[s]);
+            }
+            else
+            {
+                System.out.print(arr2[s]+" Odd nubmers");
+            }
+        }
+
+
+
+        
+
+
+
+
+
+
+    }}
