f7dba9ce03548fbe478971201768c0a3809eb66d
edikV <edward.vishnivetzki@gmail.com>
2022-01-22 17:43:14 +0200

    new

--- training/Introduction/src/Locators.java
+++ training/Introduction/src/Locators.java
@@ -1,14 +1,28 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Locators {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "c:/Users/edwardv/selenium/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("a@bcd.com");
         driver.findElement(By.name("inputPassword")).sendKeys("123456");
-        driver.findElement(By.className("submit")).click();
+        driver.findElement(By.className("signInBtn")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        System.out.println(driver.findElement(By.cssSelector(".error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Edward");
+
+
+        //================================================================//
+        //driver.findElement(By.className("btn-mute")).click();
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //driver.findElement(By.name("facebook")).click();
 
     }
 }
